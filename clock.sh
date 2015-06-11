dc3339875c80fb9052cafe34ee3f3765dee56a3a
Mahesh Baheti <mahsh.baheti@gmail.com>
2015-06-11 13:51:57 +0900

    Modularize

--- clock.sh
+++ clock.sh
@@ -9,5 +9,21 @@ else
     timezone="$1"
 fi
 
-echo "$timezone"
-while true; do tput clear;TZ=$timezone date +"%H : %M" | figlet ;TZ=$timezone cal; sleep 1; done
+display_time () {
+  tput clear
+  TZ=$timezone date +"%H : %M" | figlet
+  TZ=$timezone cal
+}
+
+display_timezone () {
+  cols=`tput cols`
+  printf "%*s\n" $(((${#timezone}+$cols)/2)) "TZ = $timezone"
+}
+
+while true
+  do
+    tput civis        # Hide the cursor
+    display_time      # Display clock and calendar
+    display_timezone  # display timezone aligned in center
+    sleep 1
+done
